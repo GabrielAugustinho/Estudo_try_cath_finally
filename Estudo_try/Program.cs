6e9e6626346066396ceb2484306a4d1b4da70c97
GabrielAugustinho <gabriel.aug.alves@outlook.com>
2021-05-10 18:09:59 -0300

    Programa de hotel sem o método try, tratamento a mão

--- Estudo_try/Program.cs
+++ Estudo_try/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Estudo_try.Entities;
 
 namespace Estudo_try
 {
@@ -7,25 +8,47 @@ namespace Estudo_try
     {
         static void Main(string[] args)
         {
-            FileStream fs = null;
-            try
+            Console.Write("Room number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if(checkOut <= checkIn)
             {
-                fs = new FileStream(@"C:\temp\data.txt", FileMode.Open);
-                StreamReader sr = new StreamReader(fs);
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
+                Console.WriteLine("Error in reservation: Check-out date must be later than Check-in.");
             }
-            catch(FileNotFoundException e)
+            else
             {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if(fs != null)
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+
+                Console.WriteLine("Reservation: " + reservation);
+                Console.WriteLine();
+
+                Console.WriteLine("Enter data to update reservation:");
+                Console.WriteLine("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkIn < now || checkOut < now)
                 {
-                    fs.Close();
+                    Console.WriteLine("Error in reservation: Reservation ddates must be future dates");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Error in reservation: Check-out date muste be later than check-in");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation: " + reservation);
                 }
             }
+
+            
         }
     }
 }
